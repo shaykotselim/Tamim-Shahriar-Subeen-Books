4b82b31c89faba84e99165aff0b2a8d1d4286dcb
shaykotselim <hossainshaykot@gmail.com>
2023-12-11 18:44:46 +0600

    added program 6

---
+++ Computer-Programming-Part-1-by-Tamim-Shahriar-Subeen/Chapter-4-Loop/program-4.6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>;
+
+using namespace std;
+
+int main(){
+
+    int n = 5;
+    int i = 1;
+
+    while (i <= 10){
+
+        cout << n  << " x " << i << " = "<< n*i <<endl;
+        i = i + 1;
+    }
+    cin.get();
+
+    return 0;
+}
