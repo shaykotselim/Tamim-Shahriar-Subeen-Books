0b344b52da237652391f3f7f1481618771a6116a
shaykotselim <hossainshaykot@gmail.com>
2023-12-12 10:02:01 +0600

    added program 12 13

---
+++ Computer-Programming-Part-1-by-Tamim-Shahriar-Subeen/Chapter-4-Loop/program-4.12.cpp
@@ -0,0 +1,18 @@
+#include <iostream>;
+
+using namespace std;
+
+int main (){
+
+    int a, b , c;
+    for (a = 1; a <= 3; a++){
+        for(b =1; b <=3; b++){
+            for(c =1 ; c <= 3; c++){
+                cout << a << " " << b <<" " <<c <<endl;
+            }
+        }
+    }
+
+    cin.get();
+    return 0;
+}
