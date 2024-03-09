e3c33b8788e9bfdcfd3d76818c90eba10137292b
shaykotselim <hossainshaykot@gmail.com>
2024-03-09 13:46:58 +0600

    added progra9, 11 c++

---
+++ Computer-Programming-Part-1-by-Tamim-Shahriar-Subeen/Chapter-6 Array/program-6.9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>;
+
+using namespace std;
+
+int main(){
+    int ara[] = {10, 20, 30, 40, 50,60, 70, 80, 90, 100};
+
+    int ara2[10];
+    int i, j;
+
+    for (i = 0, j = 9; i < 10; i++, j--){
+        ara2[j] = ara[i];
+    }
+    for (i = 0; i < 10; i++){
+        ara[i] = ara2[i];
+    }
+    for (i = 0; i < 10 ; i++){cout << ara[i] << endl;}
+
+
+
+return 0;
+}
