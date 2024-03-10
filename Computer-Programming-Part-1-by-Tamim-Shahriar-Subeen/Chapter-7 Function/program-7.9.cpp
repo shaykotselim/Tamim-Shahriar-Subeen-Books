f06493162a114f191652fb4fa1d0d6784cceb428
shaykotselim <hossainshaykot@gmail.com>
2024-03-10 15:45:04 +0600

    added program 8, 9

---
+++ Computer-Programming-Part-1-by-Tamim-Shahriar-Subeen/Chapter-7 Function/program-7.9.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>;
+#include <conio.h>;
+using namespace std;
+void test_fnc (int ara[]){
+    ara[0] = 100;
+    return ;
+
+}
+int main (){
+     int ara [] = {1, 2, 3, 4, 5};
+    cout << ara[0] << endl;
+
+    test_fnc(ara);
+
+    cout << ara[0];
+getch();
+}
