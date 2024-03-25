8f00bfad432085e3dec2761f52cc2bac7d9a50ee
shaykotselim <hossainshaykot@gmail.com>
2024-03-25 16:59:49 +0600

    added program 9.9

---
+++ Computer-Programming-Part-1-by-Tamim-Shahriar-Subeen/Chapter-9-String/program-9.9.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main (){
+   char s[1002], ch;
+    int i , length, is_word_started;
+
+    gets(s);
+    length = strlen(s);
+    is_word_started = 0;
+
+    for(i = 0; i < length; i++){
+        if(s[i] >= 'a' && s[i] <= 'z'){
+            if(is_word_started == 0){
+                is_word_started = 1;
+                ch = 'A' + s[i] - 'a';
+//                printf("%c", ch);
+                cout << ch;
+            }
+            else {
+//                printf("%c", s[i]);
+                cout << s[i];
+            }
+        }
+        else if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' && s[i] <= '9')){
+            if(is_word_started == 0){
+                is_word_started = 1;
+            }
+//            printf("%c", s[i]);
+            cout << s[i];
+        }
+        else {
+            if (is_word_started == 1){
+                is_word_started = 0;
+//                printf("\n");
+                cout << endl;
+            }
+        }
+    }
+//    printf("\n");
+cout << endl;
+
+}
